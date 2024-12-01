df552f836a51aef7a822ae5ae1881e939400504a
Tom Ruloff <23336472+TruLov@users.noreply.github.com>
2024-12-01 18:34:49 +0100

    cleanup

--- 2024/01/main.go
+++ 2024/01/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -11,59 +10,20 @@ import (
 )
 
 func main() {
-	raw, err := read("input.txt")
+	lines, err := utils.ReadLines("input.txt")
 	if err != nil {
 		fmt.Println("Error reading input file:", err)
 		return
 	}
 
-	a, b, err := parse(raw)
+	a, b, err := parse(lines)
 	if err != nil {
 		fmt.Println("Error parsing input:", err)
 		return
 	}
 
-	result := part1(a, b)
-	fmt.Println(result)
-
-	result2 := part2(a, b)
-	fmt.Println(result2)
-
-}
-
-func read(path string) (string, error) {
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-	return string(data), nil
-}
-
-func parse(data string) ([]int, []int, error) {
-	lines := strings.Split(string(data), "\n")
-	if lines[len(lines)-1] == "" {
-		lines = lines[:len(lines)-1]
-	}
-
-	var a, b []int
-	for _, line := range lines {
-		parts := strings.Fields(line) // Split the line by whitespace
-
-		num1, err1 := strconv.Atoi(parts[0])
-		num2, err2 := strconv.Atoi(parts[1])
-		if err1 != nil || err2 != nil {
-			return nil, nil, fmt.Errorf("invalid numbers in line: %s", line)
-		}
-
-		a = append(a, num1)
-		b = append(b, num2)
-	}
-
-	// Sort the arrays
-	sort.Ints(a)
-	sort.Ints(b)
-
-	return a, b, nil
+	fmt.Println(part1(a, b))
+	fmt.Println(part2(a, b))
 }
 
 func part1(a []int, b []int) int {
@@ -88,4 +48,26 @@ func part2(a []int, b []int) int {
 	}
 
 	return result
+}
+
+func parse(lines []string) ([]int, []int, error) {
+	var a, b []int
+	for _, line := range lines {
+		parts := strings.Fields(line) // Split the line by whitespace
+
+		num1, err1 := strconv.Atoi(parts[0])
+		num2, err2 := strconv.Atoi(parts[1])
+		if err1 != nil || err2 != nil {
+			return nil, nil, fmt.Errorf("invalid numbers in line: %s", line)
+		}
+
+		a = append(a, num1)
+		b = append(b, num2)
+	}
+
+	// Sort the arrays
+	sort.Ints(a)
+	sort.Ints(b)
+
+	return a, b, nil
 }
--- 2024/utils/utils.go
+++ 2024/utils/utils.go
@@ -1,8 +1,27 @@
 package utils
 
+import (
+	"os"
+	"strings"
+)
+
 func Abs(x int) int {
 	if x < 0 {
 		return -x
 	}
 	return x
+}
+
+func ReadLines(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(data), "\n")
+	if lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+
+	return lines, nil
 }
