d5c1b547ca1d0879b97704b141c1c3a8ec43d64a
Tom Ruloff <23336472+TruLov@users.noreply.github.com>
2024-12-02 10:59:52 +0100

    2024/02 curry

--- 2024/02/main.js
+++ 2024/02/main.js
@@ -4,6 +4,41 @@ export function part1(data) {
         .reduce((acc, val) => acc + (val ? 1 : 0), 0);
 }
 
+export function part1_curry(data) {
+    const reports = parse(data);
+
+    const diff = (nums) => nums.map((num, i) => num - nums[i + 1]).slice(0, -1);
+    const checker = (allowed) => (vals) => vals.reduce((p, c) => p && allowed.includes(c), true);
+    const inc = checker([1, 2, 3]);
+    const dec = checker([-1, -2, -3]);
+    const is_safe = (diff) => (checker1, checker2) => checker1(diff) || checker2(diff);
+
+    return reports
+        .map((report) => is_safe(diff(report))(inc, dec))
+        .reduce((acc, val) => acc + (val ? 1 : 0), 0);
+}
+
+export function part1_sets(data) {
+    const lines = parse(data);
+
+    let result = 0;
+    for (let i = 0; i < lines.length; i++) {
+        const safe_incline = new Set([1, 2, 3]);
+        const safe_decline = new Set([-1, -2, -3]);
+        const line = lines[i];
+
+        for (let j = 1; j < line.length; j++) {
+            const elevation = line[j] - line[j - 1];
+            safe_incline.add(elevation);
+            safe_decline.add(elevation);
+        }
+
+        if (safe_incline.size === 3 || safe_decline.size === 3) result++;
+    }
+
+    return result;
+}
+
 export function part2(data) {
     return parse(data)
         .map((line) => ({ ...check_report(line), line }))
@@ -69,25 +104,4 @@ function parse(data) {
         .trim()
         .split('\n')
         .map((line) => line.split(' ').map(Number));
-}
-
-function part1_sets(data) {
-    const lines = parse(data);
-
-    let result = 0;
-    for (let i = 0; i < lines.length; i++) {
-        const safe_incline = new Set([1, 2, 3]);
-        const safe_decline = new Set([-1, -2, -3]);
-        const line = lines[i];
-
-        for (let j = 1; j < line.length; j++) {
-            const elevation = line[j] - line[j - 1];
-            safe_incline.add(elevation);
-            safe_decline.add(elevation);
-        }
-
-        if (safe_incline.size === 3 || safe_decline.size === 3) result++;
-    }
-
-    return result;
 }
