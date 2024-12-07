0b1d3f1abba5b398f8257fba2ab42cbaf2f2f146
Tom Ruloff <23336472+TruLov@users.noreply.github.com>
2024-12-07 11:16:29 +0100

    add benching

--- 2024/cli.js
+++ 2024/cli.js
@@ -1,70 +1,110 @@
-import { Command } from "commander";
-import { dirname, join } from "path";
-import fs from "fs";
-import { fileURLToPath } from "url";
-// import pkg from "./package.json" with { type: "json" };
+import { Command } from 'commander';
+import { dirname, join } from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { micro_bench, iqr_bench } from './utils/bench.js';
 
 const program = new Command();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const read_input_data = (day) => {
-  const input_path = join(__dirname, day, "input.txt");
-  if (fs.existsSync(input_path)) {
-    return fs.readFileSync(input_path, "utf-8"); // Read the file as a string
-  } else {
-    console.error(`Input file for Day ${day} is missing.`);
-    process.exit(1);
-  }
-};
-
 program
-  .name("aoc")
-  .description("Run Advent of Code solutions")
-  .version("1.0.0")
-  // .version(pkg.version)
-  .argument("<day>", "Day of Advent of Code to run (1-25)")
-  .action(async (dayArg) => {
-    const dayNumber = parseInt(dayArg, 10);
-    if (isNaN(dayNumber) || dayNumber < 1 || dayNumber > 25) {
-      console.error("Invalid day. Please provide a number between 1 and 25.");
-      process.exit(1);
-    }
+    .name('aoc')
+    .description('Run Advent of Code solutions')
+    .version('1.0.0')
+    .argument('<day>', 'Day of Advent of Code to run (1-25)')
+    .action(async (day_arg) => {
+        const { day, day_path } = parse_day_arg(day_arg);
+        const { module, input } = await read_day(day_path);
 
-    const day = dayNumber.toString().padStart(2, "0");
-    const day_path = join(__dirname, day, "main.js");
+        try {
+            if (typeof module.part1 === 'function' && typeof module.part2 === 'function') {
+                const result1 = module.part1(input);
+                console.log(`${result1}`);
+                const result2 = module.part2(input);
+                console.log(`${result2}`);
+            } else {
+                program.error(`Day ${day} must export two functions: 'part1' and 'part2'.`);
+            }
+        } catch (err) {
+            program.error(`Failed to run Day ${day}: ${err.message}`, {
+                exitCode: 1,
+                code: 'aoc.runtime_error',
+            });
+        }
+    })
+    .command('bench <day>')
+    .description('Benchmark Advent of Code solutions')
+    .action(async (day_arg) => {
+        const { day, day_path } = parse_day_arg(day_arg);
+        const { module, input } = await read_day(day_path);
 
-    if (!fs.existsSync(day_path)) {
-      console.error(
-        `Day ${day} does not exist or is missing an main.js file.`
-      );
-      process.exit(1);
-    }
+        console.log(`Starting benchmarks for Day ${day}...`);
 
-    try {
-      const day_module = await import(day_path);
-      const input_data = read_input_data(day);
-
-      if (
-        typeof day_module.part1 === "function" &&
-        typeof day_module.part2 === "function"
-      ) {
-        const result1 = day_module.part1(input_data);
-        console.log(`${result1}`);
-
-        const result2 = day_module.part2(input_data);
-        console.log(`${result2}`);
-      } else {
-        console.error(
-          `Day ${day} must export two functions: 'part1' and 'part2'.`
-        );
-      }
-    } catch (err) {
-      console.error(`Failed to run Day ${day}:`, err.message);
-      process.exit(1);
-    }
-  });
+        if (typeof module.part1 === 'function' && typeof module.part2 === 'function') {
+            const iterations = 100;
+            const result1_micro = micro_bench(iterations, module.part1, input);
+            console.log(`avarage time part 1: ${result1_micro} ms`);
+            const result2_micro = micro_bench(iterations, module.part2, input);
+            console.log(`avarage time part 2: ${result2_micro} ms`);
+            
+            const result1_iqr = iqr_bench(iterations, module.part1, input);
+            console.log(`iqr bench part 1:    ${result1_iqr} ms`);
+            const result2_iqr = iqr_bench(iterations, module.part2, input);
+            console.log(`iqr bench part 2:    ${result2_iqr} ms`);
+            
+        } else {
+            program.error(`Day ${day} must export two functions: 'part1' and 'part2'.`);
+        }
+    });
 
 program.parse(process.argv);
 
+function parse_day_arg(day_arg) {
+    const day_int = parseInt(day_arg);
+    if (isNaN(day_int) || day_int < 1 || day_int > 25) {
+        program.error('Invalid day. Please provide a number between 1 and 25.', {
+            exitCode: 1,
+            code: 'aoc.invalid_argument',
+        });
+    }
+
+    const day = day_int.toString().padStart(2, '0');
+    const day_path = join(__dirname, day);
+
+    if (!fs.existsSync(day_path)) {
+        program.error(`Day ${day} does not exist`, {
+            exitCode: 1,
+            code: 'aoc.file_not_found',
+        });
+    }
+
+    return { day, day_path };
+}
+
+async function read_day(day_path) {
+    try {
+        const input = read_input_data(day_path);
+        const module_path = join(day_path, 'main.js');
+        const module = await import(module_path);
+        return { module, input };
+    } catch (e) {
+        program.error(`Failed to read inputs for Day ${day}: ${err.message}`, {
+            exitCode: 1,
+            code: 'aoc.runtime_error',
+        });
+    }
+}
+
+function read_input_data(day_path) {
+    const input_path = join(day_path, 'input.txt');
+    if (fs.existsSync(input_path)) {
+        return fs.readFileSync(input_path, 'utf-8');
+    } else {
+        program.error(`Input file for Day ${day} is missing.`, {
+            exitCode: 1,
+            code: 'aoc.file_not_found',
+        });
+    }
+}
