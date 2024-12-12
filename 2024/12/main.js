ece203706234dcdf291a940c93580afcca9f13da
Tom Ruloff <23336472+TruLov@users.noreply.github.com>
2024-12-12 09:57:41 +0100

    2024/12 - bool map instead of visited set

--- 2024/12/main.js
+++ 2024/12/main.js
@@ -1,23 +1,24 @@
 export function part1(data) {
-    const { grid, is_oob } = parse(data);
+    const { grid, cols, rows, is_oob } = parse(data);
+    const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
 
     const result = [];
-    const global_visited = new Set();
 
     // find all 'not visited' patches
     for (let y = 0; y < grid.length; y++) {
         for (let x = 0; x < grid[y].length; x++) {
-            const name = `${x},${y}`;
-            if (global_visited.has(name)) continue;
-            // found new patch
-            global_visited.add(name);
+            if (visited[y][x]) continue;
+
+            // Found new patch
+            visited[y][x] = true;
 
             let perimeter = 0;
             const type = grid[y][x];
             const patch_area = new Set(); // local visited
             patch_area.add(`${x},${y}`);
-            const queue = [[x, y]];
+
             // bfs
+            const queue = [[x, y]];
             while (queue.length) {
                 const [px, py] = queue.shift();
                 let fences = 0;
@@ -34,7 +35,7 @@ export function part1(data) {
                             ++fences;
                         } else {
                             patch_area.add(`${dx},${dy}`);
-                            global_visited.add(`${dx},${dy}`);
+                            visited[dy][dx] = true;
                             queue.push([dx, dy]);
                         }
                     });
@@ -53,36 +54,39 @@ function parse(data) {
         .trim()
         .split('\n')
         .map((row) => row.split(''));
-    const grid_length = grid.length;
-    const grid_width = grid[0].length;
-    const is_oob = (x, y) => x < 0 || y < 0 || x >= grid_width || y >= grid_length;
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const is_oob = (x, y) => x < 0 || y < 0 || x >= cols || y >= rows;
     return {
         grid,
+        cols,
+        rows,
         is_oob,
     };
 }
 
-
 export function part2(data) {
-    const { grid, is_oob } = parse(data);
-
-    const visited = new Set();
+    const { grid, cols, rows, is_oob } = parse(data);
+    const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
 
     let total_price = 0;
 
+    // find all 'not visited' patches
     for (let y = 0; y < grid.length; y++) {
         for (let x = 0; x < grid[y].length; x++) {
             const name = `${x},${y}`;
-            if (visited.has(name)) continue;
-            // found new area
-            visited.add(name);
-            const type = grid[y][x];
+            if (visited[y][x]) continue;
 
+            // found new area
+            visited[y][x] = true;
+			
+            const edge_map = new Map();
+            const type = grid[y][x];
             const patch_area = new Set();
             patch_area.add(`${x},${y}`);
+			
+			// bfs
             const queue = [[x, y]];
-            const edge_map = new Map();
-
             while (queue.length) {
                 const [px, py] = queue.shift();
 
@@ -112,7 +116,7 @@ export function part2(data) {
                             }
                         } else {
                             patch_area.add(`${dx},${dy}`);
-                            visited.add(`${dx},${dy}`);
+                            visited[dy][dx] = true;
                             queue.push([dx, dy]);
                         }
                     });
